e6446ea7fe298265d69aa8c33a6e18bd634bee0a
ohntrbl <158980133+ohntrbl@users.noreply.github.com>
2024-07-15 01:20:18 +0900

    Update background.js

--- momentum.ver.4/js/background.js
+++ momentum.ver.4/js/background.js
@@ -1,10 +1,13 @@
 const images = [
-    "2.jpeg",
-    "3.jpeg",
-    "4.jpeg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg".
+    "4.jpg",
+    "5.jpg",
     "6.jpeg",
     "7.jpeg",
-    "8.jpeg",
+    "8.jpg",
+    "9.jpg",
   ];
   
   const chosenImage = images[Math.floor(Math.random() * images.length)];
