ff90f25a9ea2c9dc3127337eede47a19f75bf245
ohntrbl <158980133+ohntrbl@users.noreply.github.com>
2024-07-14 21:37:07 +0900

    Update greetings.js

--- momentum.ver.4/js/greetings.js
+++ momentum.ver.4/js/greetings.js
@@ -1,31 +1,54 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
-
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY ="username";
-
-function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+#greeting {
+  text-align: center;
+  font-size: 35px;
+  color: rgb(255, 255, 255, 0.9);
+  margin-top: 30px;
+  margin-bottom: 20px;
+  text-shadow: 2px 2px 1px rgba(94, 94, 94, 0.1);
 }
 
-function paintGreetings(username) {
-    greeting.innerText = `Vamos, ${username}!`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+#login-form {
+  text-align: center;
+  margin-bottom: 20px;
+  margin-top: 30px;
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-if (savedUsername === null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit); 
-}   else {
-    paintGreetings(savedUsername);
+#login-form input {
+  margin-top: 10px;
+  text-align: center;
+  color: rgb(255, 255, 255, 0.9);
+  font-size: 20px;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  border-bottom: 2px solid;
+  border-bottom-color: aliceblue;
+  padding-bottom: 5px;
 }
 
+/* #login-form button {
+  text-align: center;
+  color: transparent;
+  font-size: none;
+  background-color: transparent;
+  border: none;
+} */
 
-//localStorage.removeItem('username'); <<<<<콘솔에 입력하면 초기화 된다!
+#logout-button {
+  position: absolute;
+  font-size: 15px;
+  text-align: left;
+  color: rgb(255, 255, 255, 0.2);
+  margin-top: 28.5em;
+  margin-left: 65px;
+  background-color: transparent;
+  border: none;
+}
+
+/* #login2 input {
+  text-align: center;
+  color: transparent;
+  font-size: none;
+  background-color: transparent;
+  border: none;
+} */
